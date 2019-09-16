265e5f3cdbcfd83eb00a87c881a92622bc6b3602
prakash karena <prakash.raoinfotech@gmail.com>
2019-09-16 16:34:07 +0530

    server changes commit

---
+++ Frontend/build/precache-manifest.edd696c01511f075c2ea4c3b05477455.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab63539778fd1f426ae74e62efe7906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba1f29e70a7197881f0",
+    "url": "/static/js/2.a7594f52.chunk.js"
+  },
+  {
+    "revision": "b8dfc1996972e1ff13c7",
+    "url": "/static/js/main.18332922.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
